1d87cc00de98c9bd5161662807bf9d948ca3c9e7
RDSMatheus <matheus_rds1998@hotmail.com>
2025-03-01 22:08:55 -0300

    mongodb atlas

--- src/connection/connection.ts
+++ src/connection/connection.ts
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
 
 export async function main() {
-  await mongoose
-    .connect('mongodb://127.0.0.1:27017/course-platform')
-    .then(() => {
-      console.log('Conectado ao MongoDB');
-    })
-    .catch((error) => {
-      console.error('Erro ao conectar ao MongoDB', error);
-    });
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/course-platform');` if your database has auth enabled
+  try {
+    const mongoUri = process.env.MONGODB_URI;
+    if (!mongoUri) {
+      throw new Error('MONGODB_URI is not defined');
+    }
+    await mongoose.connect(mongoUri);
+    console.log('Conectado ao MongoDB');
+  } catch (error) {
+    console.error('Erro ao conectar ao MongoDB', error);
+  }
 }
